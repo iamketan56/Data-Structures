e1d02aea3a4c710ebbad54fcb4eae4d815608424
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-11-09 08:30:17 +0530

    Create postfixevaluvation.cpp

---
+++ Stack/postfixevaluvation.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class stackk
+{
+    public:
+    int top;
+    int size;
+    char *s;
+};
+stackk so;
+int main()
+{
+    char exp[]=""
+    /*
+    use digits
+    */
+}
