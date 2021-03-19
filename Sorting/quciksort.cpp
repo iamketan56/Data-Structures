a4505fc72c2f7f6e8f69491a4f68072c7d15f3e0
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2021-03-19 11:51:03 +0530

    Update quciksort.cpp

--- Sorting/quciksort.cpp
+++ Sorting/quciksort.cpp
@@ -1,25 +1,47 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
-void swap(int *a, int *b)
+int partition(int l, int h, int a[])
 {
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
-int partion(int A[], int l, int h)
-{
-    int pivot = A[h];
-    int pindex = l;
-    for (int i = l; i < h; i++)
+    int pivot = a[l];
+    int i = l;
+    int j = h;
+    while (i < j)
     {
-        if (A[i] <= pivot)
+        do
         {
-            swap(A[i])
+            i++;
+        } while (a[i] <= pivot);
+        do
+        {
+            j--;
+        } while (a[j] > pivot);
+        if (i < j)
+        {
+            swap(a[i], a[j]);
         }
+        else
+        {
+            swap(a[l], a[j]);
+        }
+    }
+    return j;
+}
+void quiksort(int l, int h, int a[])
+{
+    if (l < h)
+    {
+        int j = partition(l, h, a);
+        quiksort(l, j, a);
+        quiksort(j + 1, h, a);
     }
 }
 int main()
 {
     int A[] = {10, 16, 8, 12, 15, 6, 3, 9, 5};
-    cout << partion(A, 0, 9);
+    quiksort(0, 9, A);
+    for (int i = 0; i < 9; i++)
+    {
+        cout << A[i] << " ";
+    }
 }
